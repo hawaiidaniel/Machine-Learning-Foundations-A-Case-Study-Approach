4a6b399f9d21e10eb62ce227e6c14f160844501a
hawaiidaniel <weigz10@gmail.com>
2016-03-09 16:15:53 -1000

    Update Regression.R

--- Regression.R
+++ Regression.R
@@ -14,3 +14,57 @@ mod_crime_new=lm(HousePrice ~ CrimeRate,data=dat_new)
 #   stat_smooth(method="lm")
 plot(HousePrice~CrimeRate,data=dat_new)
 abline(mod_crime_new)
+
+##Regression Week 1: Simple Linear Regression Assignment##
+
+training=read.csv("kc_house_train_data.csv")
+mod=lm(price~sqft_living, data=training)
+intercept=mod$coefficients[[1]]
+slope=mod$coefficients[[2]]
+predicted_output=data.frame()
+for (i in seq_along(training$id)) {
+  predicted_output=rbind(predicted_output,(intercept+slope*training$sqft_living[i]))
+}
+price_6=intercept+slope*2650
+avg=mean(training$price)
+RSS=0
+for (i in 1:17384) {
+  RSS=RSS+((predicted_output[i,]-avg)^2)
+}
+anova(mod)
+
+#800000=intercept+slope*sqft
+estimated_sqft=(800000-intercept)/slope
+
+
+training=read.csv("kc_house_train_data.csv")
+testing=read.csv("kc_house_test_data.csv")
+mod=lm(price~sqft_living, data=training)
+intercept=mod$coefficients[[1]]
+slope=mod$coefficients[[2]]
+predicted_output=data.frame()
+for (i in 1:4229) {
+  predicted_output=rbind(predicted_output,(intercept+slope*testing$sqft_living[i]))
+}
+avg=mean(testing$price)
+RSS=0
+for (i in 1:4229) {
+  RSS=RSS+((predicted_output[i,]-avg)^2)
+}
+#RSS=2.75555e+14
+
+training=read.csv("kc_house_train_data.csv")
+testing=read.csv("kc_house_test_data.csv")
+mod=lm(price~bedrooms, data=training)
+intercept=mod$coefficients[[1]]
+slope=mod$coefficients[[2]]
+predicted_output=data.frame()
+for (i in seq_along(testing$id)) {
+  predicted_output=rbind(predicted_output,(intercept+slope*testing$bedrooms[i]))
+}
+avg=mean(testing$price)
+RSS=0
+for (i in 1:4229) {
+  RSS=RSS+((predicted_output[i,]-avg)^2)
+}
+#RSS=7.184082e+13
